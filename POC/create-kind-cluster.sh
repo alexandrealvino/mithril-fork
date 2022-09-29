64c7379918cd2db6239b4dc03925099f1f9f6838
Alexandre Alvino <alexandre.alvino@hpe.com>
2022-09-29 11:46:42 -0300

    updating csi spec in istio helm charts
    Signed-off-by: Alexandre Alvino <alexandre.alvino@hpe.com>

--- POC/create-kind-cluster.sh
+++ POC/create-kind-cluster.sh
@@ -19,6 +19,8 @@ kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 #nodes:
 #- role: control-plane
+#- role: worker
+#- role: worker
 #  kubeadmConfigPatches:
 #  - |
 #    kind: InitConfiguration
--- soter/deploy.sh
+++ soter/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 kubectl apply -f spire/spire-server.yaml
-kubectl apply -f spire/spire-agent.yaml
+kubectl apply -f spire/spire-agent-test.yaml
 
 kubectl rollout status daemonset -n spire spire-agent
 
 kubectl apply -f istio/istio-base-updated.yaml
 kubectl apply -f istio/istio-istiod-updated.yaml
 kubectl apply -f istio/istio-ingress-updated.yaml
+
+kubectl label namespace default istio-injection=enabled --overwrite
